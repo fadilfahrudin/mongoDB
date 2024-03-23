b0fe8f5e75ca5f62755ee8f5a58b380626116a90
Fadil Fahrudin <fadilfahrudin32@gmail.com>
2024-03-23 07:14:28 +0700

    update config

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const username = 'fadilfahrudin32';
-const password = encodeURIComponent('W3@reunited085321');
+const password = encodeURIComponent('2cWflnm7X4fgTKS1');
 const dbName = 'sample_mflix';
 const uri = `mongodb+srv://${username}:${password}@cluster0.xkk5vrk.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(uri);
